caec57297fbc03c60171634a171347c614114551
fabriciowg <43095620+fabriciowg@users.noreply.github.com>
2018-11-18 18:48:33 -0300

    Codigo fuente borrado

--- CodigoFuente/gui/footer.php
+++
@@ -1,6 +0,0 @@
-<link href="../lib/bootstrap-4.1.1-dist/css/uargflow_footer.css" type="text/css" rel="stylesheet" />
-<footer class="footer">
-    <span class="oi oi-person"></span> 
-    <?= $_SESSION['usuario']->nombre; ?>
-    <a href="../uargflow/salir.php">Salir</a>
-</footer>
