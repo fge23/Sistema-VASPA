b243e24fdd427b54a586002a8acd48be835861b0
fabriciowg <43095620+fabriciowg@users.noreply.github.com>
2019-06-14 11:01:35 -0300

    Corrección de soloTexto.js para que admita ñÑ y vocales acentuadas Se agregan documentos creados previamente e inicios de Resumen de Reunión - Reanudación Formal del Proyecto.docx

--- CodigoFuente/lib/js/soloTexto.js
+++ CodigoFuente/lib/js/soloTexto.js
@@ -4,7 +4,7 @@ function Solo_Texto(e) {
     if (e.keyCode) code = e.keyCode;
     else if (e.which) code = e.which;
     var character = String.fromCharCode(code);
-    var AllowRegex  = /^[\ba-zA-Z\s-'()áéíóú]$/;
+    var AllowRegex  = /^[\ba-zA-Z\s-'()áéíóúÁÉÍÓÚñÑ]$/;
     if (AllowRegex.test(character)) return true; 
     return false; 
 }
